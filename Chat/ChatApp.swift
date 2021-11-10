a84867e05113231559406f5a409e9b7e2f0640c4
sergeibudyanskiy <sergeibudyanskiy@gmail.com>
2021-11-10 13:03:54 +0200

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Sergei Budyanskiy on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
